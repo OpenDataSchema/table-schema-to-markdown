811c821acc2219b9225313d53759971882fb3f04
Pierre Dittgen <pierre.dittgen@jailbreak.paris>
2021-04-27 15:25:49 +0200

    Improve enum values display

--- src/helpers.js
+++ src/helpers.js
@@ -2,6 +2,9 @@ module.exports = {
   formatMarkdownCode: function(s) {
     return `\`${s}\``
   },
+  formatMarkdownCodeList: function(l) {
+    return [...l.map((s) => `\`${s}\``)]
+  },
   formatMarkdownLinkOrText: function(text, options) {
     const { href } = options.hash
     return href ? `[${text}](${href})` : text
